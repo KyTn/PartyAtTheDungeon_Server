3690f7308d017d4f828cde4bb3c170aabc29d0f3
KyTn <angel.kytn@gmail.com>
2017-01-25 13:54:36 +0100

    init

---
+++ Source/PATD_Server/PATD_Server.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PATD_Server.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PATD_Server, "PATD_Server" );
---
+++ Source/PATD_Server/PATD_ServerGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "PATD_ServerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PATD_SERVER_API APATD_ServerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
