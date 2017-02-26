c5eaf023ddf25d628cc2f996b7aea778d2954f91
KyTn <angel.kytn@gmail.com>
2017-02-26 20:23:22 +0100

    esperando a David Llanso para que me ayude con una duda del averno

--- Source/PATD_Server/MapInfo/PD_MM_MapInfo.h
+++ Source/PATD_Server/MapInfo/PD_MM_MapInfo.h
@@ -2,14 +2,30 @@
 
 #pragma once
 
-/**
- * 
- */
+
+
+
+// FORWARD DECLARATIONS
+class PD_MG_LogicPosition;
+class PD_MG_StaticMap;
+class PD_MM_Room;
+
 class PATD_SERVER_API PD_MM_MapInfo
 {
 public:
 	PD_MM_MapInfo();
 	~PD_MM_MapInfo();
+
+	TArray<PD_MG_LogicPosition> allLogicPos;
+	TArray<PD_MM_Room> rooms;
+
+
+	// Devuelve un puntero al Room que tenga ese logPos. True si existe. 
+	bool RoomOf(PD_MG_LogicPosition logpos, PD_MM_Room* room);
+
+	// Inicializa el vector de rooms dado un staticMap
+	void CalculateRooms( PD_MG_StaticMap* sm );
+
 };
 
 
@@ -19,4 +35,12 @@ class PATD_SERVER_API PD_MM_Room
 public:
 	PD_MM_Room();
 	~PD_MM_Room();
+
+	TArray<PD_MG_LogicPosition> LogicPosInRoom;
+	TMap<PD_MG_LogicPosition, uint32> tiles;
+	TMap<PD_MG_LogicPosition, uint32> walls;
+	TMap<PD_MG_LogicPosition, uint32> interactuables;
+	TMap<PD_MG_LogicPosition, uint32> players;
+	TMap<PD_MG_LogicPosition, uint32> enemies;
+
 };
