09837c7eaf2cfc86fb8f553956273bd7a5e797ba
KyTn <angel.kytn@gmail.com>
2017-03-01 00:44:20 +0100

    ACharacter de los cuyons

--- Source/PATD_Server/GM_Game/LogicCharacter/PD_GM_LogicCharacter.cpp
+++ Source/PATD_Server/GM_Game/LogicCharacter/PD_GM_LogicCharacter.cpp
@@ -265,7 +265,7 @@ FString PD_GM_LogicCharacter::GetIDCharacter() { return ID_character; }
 ECharacterType PD_GM_LogicCharacter::GetTypeCharacter() { return type_character; }
 APD_GenericController* PD_GM_LogicCharacter::GetController() { return controller; }
 AMyCharacterParent* PD_GM_LogicCharacter::GetCharacterParent() { return characterParent; }
-AActor* PD_GM_LogicCharacter::GetCharacterBP() { return character_Player_BP; }
+ACharacter* PD_GM_LogicCharacter::GetCharacterBP() { return character_Player_BP; }
 PD_MG_LogicPosition* PD_GM_LogicCharacter::GetCurrentLogicalPosition() { return &currentLogicalPosition; }
 PD_MG_LogicPosition* PD_GM_LogicCharacter::GetMovingLogicalPosition() { return &movingLogicalPosition; }
 
@@ -377,7 +377,7 @@ void PD_GM_LogicCharacter::SetIDCharacter(FString nID_character) { ID_character 
 void PD_GM_LogicCharacter::SetTypeCharacter(ECharacterType nID_character) { type_character = nID_character; }
 void PD_GM_LogicCharacter::SetController(APD_GenericController* ncontroller) { controller = ncontroller; }
 void PD_GM_LogicCharacter::SetCharacterParent(AMyCharacterParent* ncharacterParent) { characterParent = ncharacterParent; }
-void PD_GM_LogicCharacter::SetCharacterBP(AActor* ncharacter_Player_BP) { character_Player_BP = ncharacter_Player_BP; }
+void PD_GM_LogicCharacter::SetCharacterBP(ACharacter* ncharacter_Player_BP) { character_Player_BP = ncharacter_Player_BP; }
 void PD_GM_LogicCharacter::SetCurrentLogicalPosition(PD_MG_LogicPosition* ncurrentLogicalPosition) { currentLogicalPosition = *ncurrentLogicalPosition; }
 void PD_GM_LogicCharacter::SetMovingLogicalPosition(PD_MG_LogicPosition* nmovingLogicalPosition) { movingLogicalPosition = *nmovingLogicalPosition; }
 void PD_GM_LogicCharacter::SetMapManager(PD_GM_MapManager* nmapManager) { mapMng = nmapManager; }
