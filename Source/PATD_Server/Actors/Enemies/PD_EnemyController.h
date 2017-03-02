c7bedb1a6d6bb219ef64ac00b67409372f14db9d
cil149 <mcil91@hotmail.com>
2017-03-02 02:50:51 +0100

    No se de donde sale este commit

--- Source/PATD_Server/Actors/Enemies/PD_EnemyController.h
+++ Source/PATD_Server/Actors/Enemies/PD_EnemyController.h
@@ -11,12 +11,26 @@
 UCLASS()
 class PATD_SERVER_API APD_EnemyController : public APD_GenericController
 {
-public:
+
 	GENERATED_BODY()
+	APD_EnemyController();
+	APD_EnemyController(const FString type);
 	
+	UPROPERTY(EditAnywhere, Category = "blackboard")
+		UBlackboardData* blackboardData;
+
+	UPROPERTY(EditAnywhere, Category = "behavior")
+		UBehaviorTree* behaviorTree;
+
+
 	virtual bool MoveTo(float x, float y) override;
 	virtual bool ActionTo(float x, float y, uint8 id_action) override;
 	virtual bool Animate(uint8 typeAnimation) override;
 	
-	
+	void StartTurn();
+
+
+
+	UFUNCTION(BlueprintCallable, Category = Behaviour)
+		bool AreEnemiesNear();
 };
