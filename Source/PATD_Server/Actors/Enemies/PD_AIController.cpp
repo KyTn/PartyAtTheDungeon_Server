33752553af14fa63d40c5612a356dcdad29cbe06
cil149 <mcil91@hotmail.com>
2017-06-05 23:43:18 +0200

    + Arreglo reconexion y paquetes largos + Tick en movimiento

--- Source/PATD_Server/Actors/Enemies/PD_AIController.cpp
+++ Source/PATD_Server/Actors/Enemies/PD_AIController.cpp
@@ -233,7 +233,7 @@ bool APD_AIController::CheckInRangeFromPositionToCharacter(PD_MG_LogicPosition p
 
 	FCollisionQueryParams paramsRay = FCollisionQueryParams();
 	paramsRay.AddIgnoredActor(GetPawn());
-	GetWorld()->LineTraceSingleByChannel(hit, iniPos, endPos, ECollisionChannel::ECC_Visibility, paramsRay);
+	GetWorld()->LineTraceSingleByChannel(hit, iniPos, endPos, ECollisionChannel::ECC_Camera, paramsRay);
 
 	if (hit.GetActor() != character->GetCharacterBP()) {
 		
