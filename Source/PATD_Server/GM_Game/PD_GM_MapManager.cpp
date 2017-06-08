81f34d2e7e4211c49af1c3660a9883e5043b8bdf
KyTnGameDev <angel.kytn@gmail.com>
2017-06-08 12:56:24 +0200

    ups

--- Source/PATD_Server/GM_Game/PD_GM_MapManager.cpp
+++ Source/PATD_Server/GM_Game/PD_GM_MapManager.cpp
@@ -1,4 +1,4 @@
-ActualLogicPosition// Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PATD_Server.h"
 #include "PD_GM_MapManager.h"
